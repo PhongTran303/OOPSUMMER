1f55616fbe4f80d3d21fe6eaa82146494b365d07
PhongTran303 <136779244+PhongTran303@users.noreply.github.com>
2023-06-16 21:21:04 +0700

    bai1

---
+++ ToaDo.java
@@ -0,0 +1,46 @@
+package bai1;
+
+public class ToaDo{
+
+	    private int x;
+	    private int y;
+	    private String name;
+
+	    public ToaDo() {
+	    }
+
+	    public ToaDo(int x, int y, String name) {
+	        this.x = x;
+	        this.y = y;
+	        this.name = name;
+	    }
+
+	    public void setX(int x) {
+	        this.x = x;
+	    }
+
+	    public int getX() {
+	        return x;
+	    }
+
+	    public void setY(int y) {
+	        this.y = y;
+	    }
+
+	    public int getY() {
+	        return y;
+	    }
+
+	    public void setName(String name) {
+	        this.name = name;
+	    }
+
+	    public String getName() {
+	        return name;
+	    }
+
+	    @Override
+	    public String toString() {
+	        return name + "(" + x + "," + y + ")";
+	    }
+	}
