16611319b87261fb7f4d2559dc657bab53dcd60f
PhongTran303 <136779244+PhongTran303@users.noreply.github.com>
2023-06-16 21:42:01 +0700

    bai1

---
+++ Main_ToaDo.java
@@ -0,0 +1,19 @@
+package bai1;
+
+public class Main_ToaDo {
+    public static void main(String[] args) {
+        ToaDo toado1 = new ToaDo();
+        toado1.setX(3);
+        toado1.setY(6);
+        toado1.setName("A");
+
+        ToaDo toado2 = new ToaDo(8, 10, "B");
+
+        System.out.println(toado1.getX()); 
+        System.out.println(toado1.getY()); 
+        System.out.println(toado1.getName()); 
+        System.out.println(toado1.toString()); 
+
+        System.out.println(toado2.toString());
+    }
+}
